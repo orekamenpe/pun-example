3cb6121cc0805e756b45c80086f21713d00b2529
orekamenpe <orekamenpe@gmail.com>
2016-05-06 11:14:01 +0200

    Fix: physics movement has to be done on FixedUpdate method

--- Assets/scripts/SoccerPlayerController.cs
+++ Assets/scripts/SoccerPlayerController.cs
@@ -19,7 +19,7 @@ public class SoccerPlayerController : Photon.MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         if (photonView.isMine)
         {
